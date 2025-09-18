2c445dd66c37b7f38340c4318a2e96db63a768bc
linking1411-netizen <linking1411@gmail.com>
2025-09-18 11:00:08 +0700

    .

---
+++ bai10ifelse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bai10ifelse {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Mời nhập mật khẩu hiện tại của bạn: " );
+        String a = sc.nextLine();
+            if (a.equals("Binh1234")){
+                System.out.println("Chào mừng bạn đến với Java");
+            }
+            else{
+                System.out.println("Đăng nhập thất bại!");
+            }
+                
+
+
+
+
+
+
+    }
+}
