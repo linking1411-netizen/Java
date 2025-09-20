188951261670247aa0dd35d4810f7d884c513580
linking1411-netizen <linking1411@gmail.com>
2025-09-20 18:03:33 +0700

    glitch

--- bai16for.java
+++ bai16for.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
-public class bai15whiletrue {
+public class bai16for {
     public static void main(String[] args){
-
-
+        for(int i = 20; i>=0;i-=4){
+            System.out.println("i= "+i);
+        }
+        int tong = 10;
+        for (int i=0;i<=100;i++){
+            tong+=i; 
+            System.out.println("tổng= " +tong);
+        }
 
 
 
