28f9db8c2b554c021e04a1e1da67bd2f3c3a33dd
linking1411-netizen <linking1411@gmail.com>
2025-10-03 09:58:53 +0700

    .

--- bai24random.java
+++ bai24random.java
@@ -2,7 +2,7 @@ import java.util.Random;
 public class bai24random {
     public static void main (String[]args){
         Random rd = new Random();
-        int soNguyen = rd.nextInt(0,51);//chạy số nguyên
+        int soNguyen = rd.nextInt(0,99);//chạy số nguyên
         System.out.println(soNguyen);
 
         double x = rd.nextDouble(100);// chạy số thực
