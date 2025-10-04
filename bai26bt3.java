914820bed7a8110a7440647be7e8a420a2511a90
linking1411-netizen <linking1411@gmail.com>
2025-10-04 13:56:23 +0700

    .

--- bai26bt3.java
+++ bai26bt3.java
@@ -4,9 +4,19 @@ public class bai26bt3{
          String str1 = "English = 78 Science = 83 Math = 68 History = 65";
         int sum = 0;
         int count = 0;
-        String[] part = str1.split(" ");
-
-
+        String[] parts = str1.split(" ");
+        for (String pt: parts){
+            System.out.println(pt);
+            try{
+                int num = Integer.parseInt(pt);
+                sum+=num;
+                count++;
+            } catch ( NumberFormatException e){
+            }
+        }
+        double tbc = (double) sum/count;
+        System.out.println("Tổng các số trong chuỗi là: "+sum);
+        System.out.println("Tổng trung bình cộng trong chuỗi là: "+tbc);
 
 
     }
