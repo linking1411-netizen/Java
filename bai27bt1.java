209b6da7a72e1cb6e3214eae323789de4137c105
linking1411-netizen <linking1411@gmail.com>
2025-10-06 16:35:36 +0700

    .

---
+++ bai27bt1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+public class bai27bt1 {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        System.out.println("Mời nhập vào số pt của mảng: ");
+        int n = sc.nextInt();
+        int[] M = new int[n];
+        System.out.println(Arrays.toString(M));
+
+
+    }
+}
