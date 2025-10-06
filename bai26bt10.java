b92ccaf04d6079977cf1b8d3235052a2903c629b
linking1411-netizen <linking1411@gmail.com>
2025-10-06 11:37:07 +0700

    .

---
+++ bai26bt10.java
@@ -0,0 +1,14 @@
+
+public class bai26bt10 {
+    public static void main (String[]args){     
+        String input = " gÀ   LẠI   lậP    tRìNh";
+    }
+    public static String chuanHoa (String input){
+        input = input.trim();
+        String[] words = input.split("\\s+");
+        for (int i = 0; i < words.length;i++){
+            
+
+        }
+    }
+}
