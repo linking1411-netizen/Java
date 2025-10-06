9941a352f4831039520f4c7b69ea943efbb9b85d
linking1411-netizen <linking1411@gmail.com>
2025-10-06 11:49:55 +0700

    .

--- bai26bt10.java
+++ bai26bt10.java
@@ -2,13 +2,17 @@
 public class bai26bt10 {
     public static void main (String[]args){     
         String input = " gÀ   LẠI   lậP    tRìNh";
+        System.out.println(chuanHoa(input));
     }
     public static String chuanHoa (String input){
         input = input.trim();
         String[] words = input.split("\\s+");
         for (int i = 0; i < words.length;i++){
-            
-
+            words[i]=words[i].toLowerCase();
+            String firstChar = words[i].substring(0,1);
+            firstChar = firstChar.toUpperCase();
+            words[i] = firstChar + words[i].substring(1);
         }
+        return String.join(" ",words);
     }
 }
