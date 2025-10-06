417d0c53cfd03644651033c38bd08ca7c4b22662
linking1411-netizen <linking1411@gmail.com>
2025-10-06 18:32:35 +0700

    .

--- bai27bt2.java
+++ bai27bt2.java
@@ -1,7 +1,19 @@
-
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
 public class bai27bt2 {
     public static void main (String[]args){
-        
-
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        System.out.println("Mời nhập vào số pt của mảng: ");
+        int n = sc.nextInt();
+        int[] M = new int[n];
+        System.out.println(Arrays.toString(M));
+        for (int i=0; i<n; i++){
+            int ngauNhien = rd.nextInt(4);
+            M[i] = ngauNhien;
+        }
+        System.out.println("Số mảng ngẫu nhiên vừa tạo ra là: ");
+        System.out.println(Arrays.toString(M));
     }
 }
