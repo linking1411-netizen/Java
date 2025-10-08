0711217e262868a09640316b7ea62ed73c2b4f10
linking1411-netizen <linking1411@gmail.com>
2025-10-08 13:46:14 +0700

    .

--- bai28list.java
+++ bai28list.java
@@ -2,9 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 public class bai28list {
     public static void main (String[]args){
-        
-
-
-
+        ArrayList<Integer> lst = new ArrayList<>();
+        ArrayList<Integer> lst2 = new ArrayList<>(5);
+        ArrayList<Integer> lst3 = new ArrayList<>(List.of(1,2,3,4,5,6));
+        System.out.println(lst);
+        System.out.println(lst2);
+        System.out.println(lst3);
+        ArrayList<Integer> lst4 = new ArrayList<>();
+        lst4.add(7);
+        lst4.add(8);
+        lst4.add(9);
+        System.out.println(lst4);
     }
 }
