573ce59d75731b1a4fc46b1fa1040ff8591d0b55
linking1411-netizen <linking1411@gmail.com>
2025-10-14 17:35:10 +0700

    .

--- TestInterface/KeThuaInterface.java
+++ TestInterface/KeThuaInterface.java
@@ -1,7 +1,7 @@
-public interface KeThuaInterface implements bai30{
+public class KeThuaInterface implements bai30 {
     @Override
     public void thongTin(String ten, String cccd, int namSinh){
-
+    }
     @Override
     public double tinhLuong(double luongNgay, int soNgayCong){
         return luongNgay*soNgayCong;
